08c2b69ab6fc851c377fdf08b7c22acaaafb671f
martin <mrosso10@gmail.com>
2018-01-22 16:08:24 -0300

    Retry false

--- app/workers/async_endpoint/async_endpoint_worker.rb
+++ app/workers/async_endpoint/async_endpoint_worker.rb
@@ -2,6 +2,8 @@ module AsyncEndpoint
   class AsyncEndpointWorker
     include Sidekiq::Worker
 
+    sidekiq_options retry: false
+
     def perform(async_request_id)
       async_request = AsyncRequest.find(async_request_id)
       async_request.execute
